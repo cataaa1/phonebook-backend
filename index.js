afa80afc985704434906e33a9a14c9e7bc8b3218
cataaa1 <catalina@marquevich.com>
2026-09-22 18:04:25 -0300

    phonebook 3.16 paso 4

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
 require('dotenv').config()
 const Person = require('./models/person')
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-
 
 const app = express()
 
-app.use(cors())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  
+  if (error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(requestLogger)
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {return req.method === 'POST' ? JSON.stringify(req.body) : ''})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -58,6 +50,7 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request,response) => {
